a94ef3515ec2354cba3655fe632ea7dd970dd487
spahbod@gmail.com <87122902474Ab!>
2024-05-15 21:19:44 +0200

    poprawki

--- TripApp/Models/AddClientToTripDTO.cs
+++ TripApp/Models/AddClientToTripDTO.cs
@@ -1,15 +1,22 @@
-﻿namespace TripApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TripApp.Models
 {
     public class AddClientToTripDTO
     {
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Telephone { get; set; }
 
+        [Required]
         public string Pesel { get; set; }
 
         public string TripName { get; set; }
