dfa68f1771c3fe94adc6cf573d08a064706ad28a
spahbod@gmail.com <87122902474Ab!>
2024-05-15 07:51:30 +0200

    Wygenerowany model

---
+++ TripApp/Context/S25132Context.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TripApp.Models;
+
+namespace TripApp.Context;
+
+public partial class S25132Context : DbContext
+{
+
+    private IConfiguration _configuration;
+    public S25132Context(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public S25132Context(DbContextOptions<S25132Context> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<ClientTrip> ClientTrips { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration["ConnectionStrings:DefaultConnection"]);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.IdClient).HasName("Client_pk");
+
+            entity.ToTable("Client", "trip");
+
+            entity.Property(e => e.IdClient).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(120);
+            entity.Property(e => e.FirstName).HasMaxLength(120);
+            entity.Property(e => e.LastName).HasMaxLength(120);
+            entity.Property(e => e.Pesel).HasMaxLength(120);
+            entity.Property(e => e.Telephone).HasMaxLength(120);
+        });
+
+        modelBuilder.Entity<ClientTrip>(entity =>
+        {
+            entity.HasKey(e => new { e.IdClient, e.IdTrip }).HasName("Client_Trip_pk");
+
+            entity.ToTable("Client_Trip", "trip");
+
+            entity.Property(e => e.PaymentDate).HasColumnType("datetime");
+            entity.Property(e => e.RegisteredAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.IdClientNavigation).WithMany(p => p.ClientTrips)
+                .HasForeignKey(d => d.IdClient)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Table_5_Client");
+
+            entity.HasOne(d => d.IdTripNavigation).WithMany(p => p.ClientTrips)
+                .HasForeignKey(d => d.IdTrip)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Table_5_Trip");
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.IdCountry).HasName("Country_pk");
+
+            entity.ToTable("Country", "trip");
+
+            entity.Property(e => e.IdCountry).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(120);
+
+            entity.HasMany(d => d.IdTrips).WithMany(p => p.IdCountries)
+                .UsingEntity<Dictionary<string, object>>(
+                    "CountryTrip",
+                    r => r.HasOne<Trip>().WithMany()
+                        .HasForeignKey("IdTrip")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("Country_Trip_Trip"),
+                    l => l.HasOne<Country>().WithMany()
+                        .HasForeignKey("IdCountry")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("Country_Trip_Country"),
+                    j =>
+                    {
+                        j.HasKey("IdCountry", "IdTrip").HasName("Country_Trip_pk");
+                        j.ToTable("Country_Trip", "trip");
+                    });
+        });
+
+        modelBuilder.Entity<Trip>(entity =>
+        {
+            entity.HasKey(e => e.IdTrip).HasName("Trip_pk");
+
+            entity.ToTable("Trip", "trip");
+
+            entity.Property(e => e.IdTrip).ValueGeneratedNever();
+            entity.Property(e => e.DateFrom).HasColumnType("datetime");
+            entity.Property(e => e.DateTo).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasMaxLength(220);
+            entity.Property(e => e.Name).HasMaxLength(120);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
